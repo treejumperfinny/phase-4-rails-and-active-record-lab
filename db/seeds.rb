883ba7dbeecdbbb9e68b2e019a5d183f278c3f7a
Aya Ramirez <ayaramz@gmail.com>
2022-06-28 19:47:41 -0400

    commit tree df635a12907ddcddac11fc8e21db1d8c2da30e97

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Grabbing Students WITH CONSENT"
+Student.create(first_name: "Dwayne", last_name: "Johnson")
+
+puts "Dun dun dun dun "
